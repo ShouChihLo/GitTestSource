32d7c556e4312b1fc291f993ac8cae90a558c757
sclo <sclo@gms.ndhu.edu.tw>
2021-01-07 10:11:35 +0800

    source commit 1

---
+++ GitTestSource/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestSource
+//
+//  Created by NDHU_CSIE on 2021/1/7.
+//  Copyright © 2021 NDHU_CSIE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
